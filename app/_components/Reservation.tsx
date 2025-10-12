a0e213f627b4dbe38292e367263622f77c662af9
Robert Young <david41596@yahoo.com>
2025-10-12 10:34:01 -0400

    Added Auth and using user data in app

--- app/_components/Reservation.tsx
+++ app/_components/Reservation.tsx
@@ -1,13 +1,17 @@
 import DateSelector from "@/app/_components/DateSelector";
 import ReservationForm from "@/app/_components/ReservationForm";
+import { auth } from "../_lib/auth";
 import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
 import { cabinType, settingsType } from "../types/types";
+import LoginMessage from "./LoginMessage";
 
 export default async function Reservation({ cabin }: { cabin: cabinType }) {
   const [settings, bookedDates] = (await Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ])) as [settingsType, Date[]];
+
+  const session = await auth();
 
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-[400px] mb-10 text-accent-400">
@@ -16,7 +20,11 @@ export default async function Reservation({ cabin }: { cabin: cabinType }) {
         bookedDates={bookedDates}
         cabin={cabin}
       />
-      <ReservationForm cabin={cabin} />
+      {session?.user ? (
+        <ReservationForm cabin={cabin} user={session.user} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 }
